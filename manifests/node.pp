4b85c3551d592dcf09aca6286ba5b37f1ed4b5d8
Nick Schuch <nick@previousnext.com.au>
2014-11-12 22:25:18 +1000

    Update node.pp

--- manifests/node.pp
+++ manifests/node.pp
@@ -8,7 +8,7 @@ class rancher::node (
 
   docker::image { 'rancher/agent': }
 
-  docker::run { "rancher_node":
+  docker::run { 'rancher_node':
     image   => 'rancher/agent',
     command => "http://${management}:8080",
     volumes => '/var/run/docker.sock:/var/run/docker.sock',
