96bd503f81899ad959bcb17956a4c2ddf008005b
Nick Schuch <nick@myschuch.com>
2014-11-12 22:11:13 +1000

    Require images.

--- manifests/management.pp
+++ manifests/management.pp
@@ -2,9 +2,12 @@
 
 class rancher::management {
 
+  docker::image { 'rancher/server': }
+
   docker::run { 'rancher_server':
-    image         => 'rancher/server',
-    expose        => [ '8080:8080' ],
+    image   => 'rancher/server',
+    expose  => [ '8080:8080' ],
+    require => Docker::Image['rancher/server'],
   }
 
 }
--- manifests/node.pp
+++ manifests/node.pp
@@ -6,10 +6,13 @@ class rancher::node (
 
   validate_string($management) 
 
+  docker::image { 'rancher/agent': }
+
   docker::run { "rancher_node_${title}":
-    image         => 'rancher/agent',
-    command       => "http://${management}:8080",
-    volumes       => '/var/run/docker.sock:/var/run/docker.sock',
+    image   => 'rancher/agent',
+    command => "http://${management}:8080",
+    volumes => '/var/run/docker.sock:/var/run/docker.sock',
+    require => Docker::Image['rancher/agent'],
   }
 
 }
