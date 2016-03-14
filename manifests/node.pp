831839cb3c579996f6cc2e502561be5557abf06b
Peter Souter <p.morsou@gmail.com>
2016-03-14 19:10:28 +0000

    Updates to use the latest agent setup from docs
    See http://docs.rancher.com/rancher/rancher-ui/infrastructure/hosts/custom/

--- manifests/node.pp
+++ manifests/node.pp
@@ -1,20 +1,27 @@
 # Register Docker Node.
-
 class rancher::node (
   $management = undef,
+  $registration_token = undef,
+  $rancher_master_port = 8080,
 ) {
 
   validate_string($management)
+  validate_string($registration_token)
+
 
   docker::image { 'rancher/agent':
     require => Package['docker'],
   }
 
   docker::run { 'rancher_node':
-    image   => 'rancher/agent',
-    command => "http://${management}:8080",
-    volumes => '/var/run/docker.sock:/var/run/docker.sock',
-    require => Docker::Image['rancher/agent'],
+    image      => 'rancher/agent',
+    privileged => true,
+    command    => "http://${management}:${rancher_master_port}/v1/scripts/${registration_token}",
+    volumes    => [
+      '/var/run/docker.sock:/var/run/docker.sock',
+      '/var/lib/rancher:/var/lib/rancher'
+    ],
+    require    => Docker::Image['rancher/agent'],
   }
 
 }
