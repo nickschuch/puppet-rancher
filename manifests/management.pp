3999d2371840ec9f134cfb5091b8b1156f4bffcd
Jonathan Gnagy <jgnagy@knuedge.com>
2016-08-02 09:03:37 -0700

    Don't delete the server on shutdown or startup...

--- manifests/management.pp
+++ manifests/management.pp
@@ -11,5 +11,9 @@ class rancher::management(
     image   => 'rancher/server',
     ports   => [ "${rancher_manager_port}:8080" ],
     require => Docker::Image['rancher/server'],
+    remove_container_on_start => false,
+    remove_volume_on_start    => false,
+    remove_container_on_stop  => false,
+    remove_volume_on_stop     => false,
   }
 }
