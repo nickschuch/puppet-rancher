517b8e802505367ad56c2c9a8b06b81ae8fb82f9
Sebastian Toepfer <sebastian@toepfers.eu>
2017-01-13 19:04:23 +0100

    prepare spec

--- spec/classes/management_spec.rb
+++ spec/classes/management_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe 'rancher::management' do
-  let(:params) {{
-    :rancher_manager_port => '8080',
-  }}
   it {
     is_expected.to contain_docker__image('rancher/server')
-    is_expected.to contain_docker__run('rancher/server').with({
-      'image'   => 'rancher/server',
-      'ports'   => ['8080:8080'],
-      'require' => 'Docker::Image[rancher/server]'
-    })
   }
+  context 'with default parameter' do
+    it {
+      is_expected.to contain_docker__run('rancher/server').with({
+        'image'   => 'rancher/server',
+        'ports'   => ['8080:8080'],
+        'require' => 'Docker::Image[rancher/server]'
+      })
+    }
+  end
+  context 'with rancher_manager_port => 9080' do
+    let(:params) {{
+      :rancher_manager_port => '9080',
+    }}
+    it {
+      is_expected.to contain_docker__run('rancher/server').with({
+        'image'   => 'rancher/server',
+        'ports'   => ['9080:8080'],
+        'require' => 'Docker::Image[rancher/server]'
+      })
+    }
+  end
 end
